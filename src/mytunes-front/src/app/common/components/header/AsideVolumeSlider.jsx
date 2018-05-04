dd67f6d7a004fb08f10cbad3cdaaad3effc9c466
Casou <casou.acc@gmail.com>
2018-05-04 23:28:26 +0200

    Add volume slider

---
+++ src/mytunes-front/src/app/common/components/header/AsideVolumeSlider.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { VolumeSlider } from "react-player-controls";
+
+import '../../../../style/components/volumeSlide.css';
+
+const AsideVolumeSlider = props => (
+    <aside id={"volumeSlider"}>
+        <VolumeSlider
+            direction={ "VERTICAL" }
+            volume={ props.volume }
+            isEnabled={ true }
+            onVolumeChange={ volume => props.onVolumeChange(volume) }
+        />
+    </aside>
+);
+
+AsideVolumeSlider.propTypes = {
+    volume : PropTypes.number.isRequired,
+    onVolumeChange : PropTypes.func.isRequired
+};
+
+export default AsideVolumeSlider;
--- src/mytunes-front/src/app/common/components/header/Lecteur.jsx
+++ src/mytunes-front/src/app/common/components/header/Lecteur.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import AsideVolumeSlider from "./AsideVolumeSlider";
 
-export const Lecteur = (props) => (
-  <section className="lecteur">
-    <div className="lecteurDiv">
-      <h1>{ props.titre ? props.titre : "Aucun titre" }</h1>
-      <h2>{ props.artiste ? props.artiste : "-" }</h2>
-    </div>
-  </section>
-);
+class Lecteur extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            volume : 1
+        }
+    }
+
+    render() {
+        return (
+            <section className="lecteur">
+                <div className="lecteurDiv">
+                    <h1>{ this.props.titre ? this.props.titre : "Aucun titre" }</h1>
+                    <h2>{ this.props.artiste ? this.props.artiste : "-" }</h2>
+                </div>
+                <AsideVolumeSlider volume={this.state.volume}
+                                   onVolumeChange={(volume) => this.setState({...this.state, volume }) } />
+            </section>
+        );
+    }
+
+}
 
 Lecteur.propTypes = {
   titre:PropTypes.string,
